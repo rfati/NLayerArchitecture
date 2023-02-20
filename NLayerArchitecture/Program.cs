5866c7e5aebbc6eb327b911ab3bf3757ff76023f
rfati <isler.rifat@gmail.com>
2023-02-20 14:41:15 +0300

    Fluent validation ui

--- NLayerArchitecture/Program.cs
+++ NLayerArchitecture/Program.cs
@@ -1,5 +1,7 @@
 ﻿using BusinessLayer;
+using BusinessLayer.ValidationRules;
 using EntityLayer.Entities;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,12 +18,26 @@ namespace NLayerArchitecture
             ProductManager pm = new ProductManager();
             Category c = new Category();
             Product p = new Product();
-            c.CategoryId = 3;
-            c.CategoryName = "Giyim";
-            cm.BLUpdate(c);
+            //c.CategoryId = 3;
+            //c.CategoryName = "Giyim";
+            //cm.BLUpdate(c);
             foreach (var item in pm.GetAll())
             {
                 Console.WriteLine("ID: " + item.ID + " - Ürün Adı: " + item.Name);
+            }
+            c.CategoryName = "DE";
+            CategoryValidator categoryValidator = new CategoryValidator();
+            ValidationResult results = categoryValidator.Validate(c);
+            if (results.IsValid)
+            {
+                cm.BLAdd(c);
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    Console.WriteLine(item.ErrorMessage);
+                }
             }
             //Category ct = new Category();
             //ct.CategoryName = "Halılar";
